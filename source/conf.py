ef1e59dfa05a0d330f519dac4f283894d9755112
ayato0211 <ayato.skycrawlers@gmail.com>
2014-04-18 21:51:02 +0900

    conf.py modify

--- source/conf.py
+++ source/conf.py
@@ -25,8 +25,8 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax']
-extensions = ['sphinxtogithub']
+extensions = ['sphinxtogithub', 'sphinx.ext.mathjax']
+mathjax_path = 'http://mathjax.connectmv.com/MathJax.js?config=default'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
